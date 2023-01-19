553d5016f45b53e66febed27de71c704fb9c0838
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-18 23:24:30 -0300

    Solução sem interface - bug fix systemout

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,9 +38,9 @@ public class Program {
         rentalService.processInvoice(cr);
 
         System.out.println("FATURA:");
-        System.out.println("Pagamento basico: " + cr.getInvoice().getBasicPayment());
-        System.out.println("Imposto: " + cr.getInvoice().getTax());
-        System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
+        System.out.println("Pagamento basico: " + String.format("%.2f",cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 
         sc.close();
     }
